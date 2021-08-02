8f9b9c755c0e1c47778c98dae158e11e562e37be
bro-alex <br.terehov@gmail.com>
2021-08-02 16:01:54 +0200

    policy

--- test/strtest/index.php
+++ test/strtest/index.php
@@ -4,7 +4,29 @@
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="subtest/style.css">
-    
+    <script type="text/javascript">
+ 
+function ValidMail() {
+    var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+    var myMail = document.getElementById('email').value;
+    var valid = re.test(myMail);
+    if (valid) output = 'Адрес эл. почты введен правильно!';
+    else output = 'Адрес электронной почты введен неправильно!';
+    document.getElementById('message').innerHTML = output;
+    return valid;
+}
+ 
+function ValidPhone() {
+    var re = /^\+[0-9]{11}$/;
+    var myPhone = document.getElementById('phone').value;
+    var valid = re.test(myPhone);
+    if (valid) output = 'Номер телефона введен правильно!';
+    else output = 'Номер телефона введен неправильно!';
+    document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
+    return valid;
+}  
+ 
+</script>
 </head>
 <body>
 <div class="top">
--- test/strtest/subtest/index.php
+++ test/strtest/subtest/index.php
@@ -1,30 +1,8 @@
-<script type="text/javascript">
- 
-function ValidMail() {
-    var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
-    var myMail = document.getElementById('email').value;
-    var valid = re.test(myMail);
-    if (valid) output = 'Адрес эл. почты введен правильно!';
-    else output = 'Адрес электронной почты введен неправильно!';
-    document.getElementById('message').innerHTML = output;
-    return valid;
-}
- 
-function ValidPhone() {
-    var re = /^\+[0-9]{11}$/;
-    var myPhone = document.getElementById('phone').value;
-    var valid = re.test(myPhone);
-    if (valid) output = 'Номер телефона введен правильно!';
-    else output = 'Номер телефона введен неправильно!';
-    document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
-    return valid;
-}  
- 
-</script>
+
 <link rel="stylesheet" href="style.css">
     <div class="form_register">
 
-    <p id="message" style="color:red"></p> 
+    <p id="message" style="color: red;"></p> 
         <form class="form" action="subtest/register.php" method="post" name="form_register"  onsubmit="return (ValidPhone()&&ValidMail())">
            
                     <div class="name"> <div class="namein">Имя: </div> 
@@ -40,7 +18,8 @@ function ValidPhone() {
 <div class="name"> <div class="namein">e-mail: </div>
                         <input id="email" class="intext" type="text" name="email" required="required" placeholder="Пример: name@mail.ru" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Введите ваш e-mail'">
 </div>
-               
+<div style="margin-bottom: 3%; font-size: 12px; text-align: justify;"><input type="checkbox" checked="checked">Согласие с нашей <a href="#">Политикой</a> защиты персональной информации</div>
+
                     <div class="inbtn">
                         
                         <input class="input" type="submit" name="btn_submit_register" value="Пройти тест" onClick="ValidMail(); ValidPhone()">
